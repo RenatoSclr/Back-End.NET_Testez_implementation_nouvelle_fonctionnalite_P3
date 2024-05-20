bc8e206d80d012ee0d6d3269ae3e567fd4eb6023
Renato Sicler <renatosicler33@gmail.com>
2024-05-20 13:09:52 +0200

    Creating Unit Tests for LanguageService Class

---
+++ DotNetEnglishP3-master/P3AddNewFunctionalityDotNetCore.Tests/LanguageServiceTests.cs
@@ -0,0 +1,55 @@
+﻿using P3AddNewFunctionalityDotNetCore.Models.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace P3AddNewFunctionalityDotNetCore.Tests.UnitTests
+{
+    public class LanguageServiceTests
+    {
+        [Fact]
+        public void SetCulture_Fr()
+        {
+            // Arrange
+            ILanguageService languageService = new LanguageService();
+            string language = "French";
+
+            // Act
+            string culture = languageService.SetCulture(language);
+
+            // Assert
+            Assert.Same("fr", culture);
+        }
+
+        [Fact]
+        public void SetCulture_En()
+        {
+            // Arrange
+            ILanguageService languageService = new LanguageService();
+            string language = "English";
+
+            // Act
+            string culture = languageService.SetCulture(language);
+
+            // Assert
+            Assert.Same("en", culture);
+        }
+
+        [Fact]
+        public void SetCulture_Es()
+        {
+            // Arrange
+            ILanguageService languageService = new LanguageService();
+            string language = "Spanish";
+
+            // Act
+            string culture = languageService.SetCulture(language);
+
+            // Assert
+            Assert.Same("es", culture);
+        }
+    }
+}
